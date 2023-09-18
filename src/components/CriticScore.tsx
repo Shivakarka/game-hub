a4c9530d36116b8024eeedd0300029176dca1f8c
shivakarka <shivakarka@gmail.com>
2023-09-18 23:30:13 +0530

    Displaying critic score in gamecards

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+
+interface CriticScoreProps {
+  score: number;
+}
+
+const CriticScore = ({ score }: CriticScoreProps) => {
+  let color = score >= 75 ? "green" : score >= 60 ? "yellow" : "";
+
+  return (
+    <Badge fontSize={"14px"} px={2} borderRadius={"4px"} colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
