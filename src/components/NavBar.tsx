bc81f158e6a93ef86e595e0abfcb6141fefbca08
shivakarka <shivakarka@gmail.com>
2023-09-18 19:19:16 +0530

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+export const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize={"60px"} />
+      <Text fontSize={"2xl"} fontWeight={"bold"}>
+        GameHub
+      </Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
