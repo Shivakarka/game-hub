fabe3e1223295a74e4d5eaf72e8ca3eeb7c35f41
shivakarka <shivakarka@gmail.com>
2023-09-19 23:14:31 +0530

    handled no image issue

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return url;
+  if (!url) return noImage;
   const croppedUrl = url.replace("/media/games/", "/media/crop/600/400/games/");
   return croppedUrl;
 };
