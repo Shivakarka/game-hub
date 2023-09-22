e9f1fe5a6ac71ca74d1d542525a6bfdcc96ce4b5
shivakarka <shivakarka@gmail.com>
2023-09-22 22:26:22 +0530

    Refactor- remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,7 +9,7 @@ import {
 import { SiNintendo } from "react-icons/si";
 import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,7 @@
 import { GameQuery } from "./../App";
 import { useQuery } from "@tanstack/react-query";
 import apiClient, { FetchResponse } from "../services/api-client";
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from "./usePlatforms";
 
 export interface Game {
   id: number;
