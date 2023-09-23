df779518ad0b482bc739a3f78f8d44715fb4aa75
shivakarka <shivakarka@gmail.com>
2023-09-24 02:54:01 +0530

    Refactor entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,7 +9,7 @@ import {
 import { SiNintendo } from "react-icons/si";
 import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client.ts";
 import genres from "../data/genres.ts";
 import ms from "ms";
-import { Genre } from "../entities/Genre.ts";
+import Genre from "../entities/Genre.ts";
 
 const apiClient = new APIClient<Genre>("/genres");
 
