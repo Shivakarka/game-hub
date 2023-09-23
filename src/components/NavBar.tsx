6ee03535459424ac00a05783115ed0a767de331f
shivakarka <shivakarka@gmail.com>
2023-09-24 02:34:44 +0530

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,21 @@ import { HStack, Image, Show, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <HStack padding={"10px"}>
       <HStack>
-        <Image src={logo} boxSize={"60px"} />
-        <Show above="lg">
-          <Text fontSize={"2xl"} fontWeight={"bold"} marginEnd={"40px"}>
-            GameHub
-          </Text>
-        </Show>
+        <Link to="/">
+          <Image src={logo} boxSize="60px" objectFit="cover" />
+        </Link>
       </HStack>
+      <Show above="lg">
+        <Text fontSize="2xl" fontWeight="bold">
+          GameHub
+        </Text>
+      </Show>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,9 +2,12 @@ import { useRef } from "react";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+
+  const navigate = useNavigate();
 
   const setSearchText = useGameQueryStore((state) => state.setSearchTerm);
 
@@ -14,6 +17,7 @@ const SearchInput = () => {
         e.preventDefault();
         if (ref.current) {
           setSearchText(ref.current.value);
+          navigate("/");
         }
       }}
     >
